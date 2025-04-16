3f9179835912ac54f6cb4ca8f0afff39e4b8d023
Artyom Vasilyev <hegelianer@gmail.com>
2025-04-16 11:13:20 +0700

    test terra

--- infra/variables.tf
+++ infra/variables.tf
@@ -8,22 +8,22 @@ variable "TF_deploy_ssh_key" {
   type        = string
 }
 
-variable "service_account_key_file" {
+variable "TF_service_account_key_file" {
   description = "Path to the service account key file (JSON)"
   type        = string
 }
 
-variable "cloud_id" {
+variable "TF_cloud_id" {
   description = "Yandex Cloud ID"
   type        = string
 }
 
-variable "folder_id" {
+variable "TF_folder_id" {
   description = "Yandex Cloud Folder ID"
   type        = string
 }
 
-variable "zone" {
+variable "TF_zone" {
   description = "Default availability zone"
   default     = "ru-central1-a"
 }
