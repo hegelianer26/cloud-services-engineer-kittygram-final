049cd72ae2ab7f218d6b2f16b19e58e4d977a55a
Artyom Vasilyev <hegelianer@gmail.com>
2025-04-16 10:31:06 +0700

    test terra

--- infra/providers.tf
+++ infra/providers.tf
@@ -6,3 +6,11 @@ terraform {
   }
   required_version = ">= 0.13"
 }
+
+
+provider "yandex" {
+  service_account_key_file = var.service_account_key_file
+  cloud_id                 = var.cloud_id
+  folder_id                = var.folder_id
+  zone                     = var.zone
+}
--- infra/variables.tf
+++ infra/variables.tf
@@ -7,3 +7,23 @@ variable "TF_deploy_ssh_key" {
   description = "SSH public key for accessing the instance by deploy user"
   type        = string
 }
+
+variable "service_account_key_file" {
+  description = "Path to the service account key file (JSON)"
+  type        = string
+}
+
+variable "cloud_id" {
+  description = "Yandex Cloud ID"
+  type        = string
+}
+
+variable "folder_id" {
+  description = "Yandex Cloud Folder ID"
+  type        = string
+}
+
+variable "zone" {
+  description = "Default availability zone"
+  default     = "ru-central1-a"
+}
